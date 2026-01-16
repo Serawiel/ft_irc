09bde4dda3ad8eb38f1e5796d3b74c7ce088a40f
Coraline Prot <cprot@student.42nice.fr>
2026-01-16 17:28:19 +0100

    ex02 de la mort

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -2,36 +2,45 @@
 # define PMERGEME_HPP
 
 # include <iostream>
+# include <set>
 # include <sstream>
+# include <utility>
 # include <vector>
-
-struct		Pair
-{
-	int		large;
-	int		small;
-};
+# include <deque>
 
 class PmergeMeVector
 {
   private:
-	std::vector<Pair> _pairs;
-	std::vector<int> _larges;
-	std::vector<int> _result;
-	int		_aloneNb;
-	bool	_hasAloneNb;
-
-	void createPairs(std::string const &str);
-	void sortLarger();
-	void insertSmaller();
-
+	std::vector<int> mergeInsertSort(std::vector<int> input);
+	std::vector<size_t> generateJacobsthal(size_t pendSize);
+	
   public:
 	PmergeMeVector();
 	PmergeMeVector(PmergeMeVector const &src);
 	PmergeMeVector const &operator=(PmergeMeVector const &src);
 	~PmergeMeVector();
 
-	void sort();
+	std::vector<int> sort(int argc, char **argv);
 };
+
+class PmergeMeDeque
+{
+  private:
+	std::deque<int> mergeInsertSort(std::deque<int> input);
+	std::deque<size_t> generateJacobsthal(size_t pendSize);
+	
+  public:
+	PmergeMeDeque();
+	PmergeMeDeque(PmergeMeDeque const &src);
+	PmergeMeDeque const &operator=(PmergeMeDeque const &src);
+	~PmergeMeDeque();
+
+	std::deque<int> sort(int argc, char **argv);
+};
+
+std::ostream& operator<<(std::ostream& os, const std::vector<int>& vec);
+
+std::ostream& operator<<(std::ostream& os, const std::deque<int>& vec);
 
 class ErrorException : public std::exception
 {
