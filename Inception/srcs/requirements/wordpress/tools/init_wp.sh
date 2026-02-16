e7a21744848ddd448ffb83bb901fe223364a0502
Coraline Prot <cprot@student.42nice.fr>
2026-02-16 19:51:58 +0100

    config container maria et wp, debut nginx

---
+++ Inception/srcs/requirements/wordpress/tools/init_wp.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+if [ ! -f /var/www/html/wp-config.php ]; then
+	echo "Configuring WordPress..."
+	cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+	sed -i "s/database_name_here/${MYSQL_DATABASE}/" /var/www/html/wp-config.php
+	sed -i "s/username_here/${MYSQL_USER}/" /var/www/html/wp-config.php
+	sed -i "s/password_here/${MYSQL_PASSWORD}/" /var/www/html/wp-config.php
+	sed -i "s/localhost/${WORDPRESS_DB_HOST}/" /var/www/html/wp-config.php
+
+	echo "WordPress configured!"
+fi
+
+echo "Starting PHP-FPM..."
+exec php-fpm7.4 --nodaemonize
