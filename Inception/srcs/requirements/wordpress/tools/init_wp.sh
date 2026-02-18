c4786dc8a6814b491a7ccd4ef495a9befc23fb5f
Coraline Prot <cprot@student.42nice.fr>
2026-02-18 13:49:36 +0100

    modif secret

--- Inception/srcs/requirements/wordpress/tools/init_wp.sh
+++ Inception/srcs/requirements/wordpress/tools/init_wp.sh
@@ -3,7 +3,7 @@ set -e
 
 # Read secrets from Docker secrets files
 MYSQL_PASSWORD=$(cat /run/secrets/db_password)
-MYSQL_USER=$(cat /run/secrets/db_user)
+MYSQL_USER=$(cat /run/secrets/credentials)
 
 if [ ! -f /var/www/html/wp-config.php ]; then
     echo "Configuring WordPress..."
